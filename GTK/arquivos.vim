4ea1f3442eb372fa25a0cc555bab2c5ca7b2d4b9
marcuswlima <marcuswlima@gmail.com>
2023-07-22 00:52:03 -0300

    [c++/Notas] Consolicadao miniprojeto GTK  On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	new file:   Makefile
    	new file:   arquivos.vim
    	deleted:    main.cpp
    	renamed:    GTKNotas.cpp -> src/GTKNotas.cpp
    	renamed:    GTKNotas.h -> src/GTKNotas.h
    	new file:   src/main.cpp

---
+++ GTK/arquivos.vim
@@ -0,0 +1,6 @@
+:edit Makefile
+:tabedit src/GTKNotas.cpp
+:49vsplit src/GTKNotas.h
+:tabedit src/main.cpp
+:tabfirst
+
---
+++ GTK/src/main.cpp
@@ -0,0 +1,10 @@
+#include "GTKNotas.h"
+
+/////////////////////////////////////////
+// Main
+/////////////////////////////////////////
+int main(int argc, char *argv[] ){
+	GTKNotas gtk(argc, argv);
+	return 0; //indica o fim do programa2
+}//main
+
